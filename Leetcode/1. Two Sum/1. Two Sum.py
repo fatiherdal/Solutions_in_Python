c9e60c044293f6e8ef937593f16f3814c4c8aad6
fatih <46056982+fatiherdal@users.noreply.github.com>
2019-01-21 20:16:15 +0100

    Update 1. Two Sum.py

--- Leetcode/1. Two Sum/1. Two Sum.py
+++ Leetcode/1. Two Sum/1. Two Sum.py
@@ -1,16 +1,16 @@
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for  idx, value in enumerate(nums):
-  
-            result = target - value
-            diff = len(nums) - len(nums[idx+1:])
-            if result in nums[idx+1:]:
+def twoSum(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    for  idx, value in enumerate(nums):
 
-                final_list = [idx, (nums[idx+1:].index(result)+diff)]
-                break
+        result = target - value
+        diff = len(nums) - len(nums[idx+1:])
+        if result in nums[idx+1:]:
 
-        return final_list
+            final_list = [idx, (nums[idx+1:].index(result)+diff)]
+            break
+
+    return final_list
