39c9b1605f57791b3206db126e987d3281e01b02
fatih <46056982+fatiherdal@users.noreply.github.com>
2019-01-26 22:37:01 +0100

    Add files via upload

---
+++ Leetcode/347. Top K Frequent Elements/347. Top K Frequent Elements.py
@@ -0,0 +1,25 @@
+def topKFrequent(self, nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: List[int]
+    """
+    freq = {}
+    for x in set(nums):
+        rep = nums.count(x)
+        if rep in freq.keys():
+            freq[rep].append(x)
+        else:
+            freq[rep] = [x]
+    key_list = sorted(freq.keys(), reverse=True)
+    result = []
+    ctrl = 0
+    for i in key_list:
+        if ctrl < k:
+            ctrl = ctrl + len(freq[i])
+            result = result + freq[i]
+        else:
+            break
+    return result
+    else:
+        return False
